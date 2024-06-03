a6f6abe536ae57a825074f257c0cd5f192ef8706
Thomas Salzmann <thomas.salzmann.privat@gmail.com>
2024-06-03 09:42:28 +0200

    Install django-stubs for mypy to work with Django
    - Without this dependency, mypy can not resolve django imports, and
      thus, throws an error for each import from 'django' library.
    
    - Configuration of dependency in mypy.ini and .pre-commit-config.yaml
      required.

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
